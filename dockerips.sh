45ee865a7081fd0a189bc3ce878b7f4a19e3a4cf
Alexander Mayorov <alexander.majorov@gmail.com>
2022-07-15 08:09:00 +0400

    Create dockerips.sh

---
+++ dockerips.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+function dockerip {
+    docker inspect -f \
+       '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
+}
+
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+#seq -s- $COLUMNS|tr -d '[:digit:]'
+
+i=0
+docker ps | while read s
+do
+  if [ 0 = $i ]
+  then
+    echo -e "| $s\tLocal IP"
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    ((i=i+1))
+  else
+    uid=$(echo $s | awk '{print $1}')
+    localip=`dockerip $uid`
+    if [ ! -z "$localip" ]
+    then
+      echo -e  "| $s\t$localip"
+    else
+      echo -e "| $s\tlocalhost"
+    fi
+  fi
+done
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+
+#EOF#
